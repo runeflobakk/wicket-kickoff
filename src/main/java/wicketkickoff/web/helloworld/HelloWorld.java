d0cbf762501cc9fd55932681e65cd50ce40ea080
Rune Flobakk <flobakk@gmail.com>
2011-05-05 15:28:47 +0200

    svada

--- src/main/java/wicketkickoff/web/helloworld/HelloWorld.java
+++ src/main/java/wicketkickoff/web/helloworld/HelloWorld.java
@@ -2,10 +2,17 @@ package wicketkickoff.web.helloworld;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.PropertyModel;
 
 public class HelloWorld extends WebPage {
 
+    private int teller = 1;
+
     public HelloWorld() {
-        add(new Label("user", "World"));
+        add(new Label("user", new PropertyModel<String>(this, "user")));
+    }
+
+    public String getUser() {
+        return "World" + teller++;
     }
 }
